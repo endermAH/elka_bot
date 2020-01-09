f8e741246391d0cbbdf3b930de5a08c3e27551ad
endermAH <gerakl00@gmail.com>
2020-01-09 23:53:35 +0300

    first stable

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
 import requests
+import time
+import random
 
 class Elochka:
-    sessionKey = 'ba2e28ec65a35b2c8824562cfd9854b7cb8b9a94'
+    sessionKey = 'ac145905ef88688480751b733397774b3551877b'
+    version = '19'
 
     mainHeaders = {
         'Host': 'elka2020-server-vk.ereality.org',
@@ -29,6 +32,12 @@ class Elochka:
         print('Elka ERROR caused: ' + error + '\nData: ')
         print(data)
 
+    def notifyMe(self, msg):
+        random_id = random.randrange(0,100000,1)
+        url = 'https://api.vk.com/method/messages.send?peer_id=225299625&random_id=' + str(random_id) + '&message=' + msg + '&access_token=85681fa2463297bc96226c3e4070e4eecf0654fdd088ec266697505cde91b86b8ef0c33303caf5ced2ae7&v=5.103'
+        responce = requests.get(url)
+        print(responce.content)
+
     def getSessionKey(self):
         url = 'https://elka2020-server-vk.ereality.org/canvas/show?api_url=https://api.vk.com/api.php&api_id=7113532&api_settings=2368775&viewer_id=225299625&viewer_type=0&sid=9090c6a092e51714593c9550cb7e74edbead0f63d9334546224f80914079665b680307feddd0d99374e2f&secret=932fa69ab4&access_token=828ebf001e29d563422c4f94e4d3a5269fa01f59cdf30f2b7b2669983dbe4fbd3806f7fcf61bbad9425ae&user_id=0&group_id=0&is_app_user=1&auth_key=a8d561b58babc3b79218936fa82a0684&language=0&parent_language=0&is_secure=1&stats_hash=1dfee73734f66879bf&ads_app_id=7113532_4d9f4d4083fdfa1e36&referrer=unknown&lc_name=92c0bd26&platform=web&hash='
         headers = {
@@ -49,7 +58,7 @@ class Elochka:
         self.sessionKey = r['sessionKey']
 
     def factoryExchange(self):
-        sign = 'e16b1c675a77b5c7d44c3cb841b2d416'
+        sign = '7437ad085d5eb02b131c52be815648cd'
         url = 'https://elka2020-server-vk.ereality.org/generator/exchange'
         headers = self.mainHeaders
         headers.update({
@@ -57,7 +66,7 @@ class Elochka:
             'Content-Length': '253',
             })
 
-        data = '{"params":{"name":"farm1"},"uid":125744,"suid":"225299625","aid":"7113532","authKey":"de7afe2878276a4091b89bd70ee1d9d1","sessionKey":"' + self.sessionKey + '","version":18,"clientPlatform":"js","sign":"' + sign + '"}'
+        data = '{"params":{"name":"farm1"},"uid":125744,"suid":"225299625","aid":"7113532","authKey":"de7afe2878276a4091b89bd70ee1d9d1","sessionKey":"' + self.sessionKey + '","version":' + self.version + ',"clientPlatform":"js","sign":"' + sign + '"}'
         # print(data)
         r = requests.post(url, data=data, headers=headers)
         r = r.json()
@@ -145,7 +154,7 @@ class Elochka:
         })
 
         data = '{"params":{"objectName":"winterMaiden","count":' + str(count) + ',"window":1},"uid":125744,"suid":"225299625","aid":"7113532","authKey":"de7afe2878276a4091b89bd70ee1d9d1","sessionKey":"' + self.sessionKey + '","version":18,"clientPlatform":"js","sign":"' + sign + '"}'
-        print(data)
+        # print(data)
         r = requests.post(url, data=data, headers=headers)
         r = r.json()
         print(r)
@@ -165,22 +174,66 @@ class Elochka:
         return data
 
     def openChest(self):
-        sign = 'f51aee17d8f95db54592f61cbd6e0c52'
+        sign = '477b818da66f35015b8356520ee12d02'
         url = 'https://elka2020-server-vk.ereality.org/chest/open'
         headers = self.mainHeaders
         headers.update({
-            'Content-Length': '250'
+            'Content-Length': '250',
             'Referer': 'ttps://elka2020-client-vk.ereality.org/?api_url=https://api.vk.com/api.php&api_id=7113532&api_settings=2368775&viewer_id=225299625&viewer_type=0&sid=dc3c436e35e89ce086568cdfab418cde7400f4c45f09255f5ea4a3a29c6d465b94ddb8f28774d93eeea5d&secret=55a81b0b30&access_token=51a0f41f89a45ed21be37d6ab0ea9c00d20f66710faedb54b33614df78d05ad632f1594f6bc47a02e20be&user_id=0&group_id=0&is_app_user=1&auth_key=a8d561b58babc3b79218936fa82a0684&language=0&parent_language=0&is_secure=1&stats_hash=1dfee73734f66879bf&ads_app_id=7113532_8abd913ee8afe00b53&referrer=unknown&lc_name=6ddc78f3&platform=web&hash='
         })
-        data = '{"params":{"chestId":2},"uid":125744,"suid":"225299625","aid":"7113532","authKey":"de7afe2878276a4091b89bd70ee1d9d1","sessionKey":"' + self.sessionKey + '","version":18,"clientPlatform":"js","sign":"' +  sign + '"}'
+        data = '{"params":{"chestId":2},"uid":125744,"suid":"225299625","aid":"7113532","authKey":"de7afe2878276a4091b89bd70ee1d9d1","sessionKey":"' + self.sessionKey + '","version":' + self.version + ',"clientPlatform":"js","sign":"' +  sign + '"}'
+        r = requests.post(url, data=data, headers=headers)
+        r = r.json()
+
+        if ('data' in r):
+            if ('award' in r['data']):
+                 award = r['data']['award']
+            else:
+                award = 'no award'
+        else:
+            award = 'no data'
+            self.__causeError(award, r)
+
+        data = {
+            'award': award,
+            'r': r
+        }
+        return data
 
 
 el = Elochka()
+timer = 0
+
+message = 'Elka bot job started.'
+el.notifyMe(message)
+
+while (True):
+    resp = el.openChest()
+    print(resp['r'])
+    if (resp['award'] == 'no award' or resp['award'] == 'no data'):
+        message = 'I\'ve tried to open the chest, but there is no award :('
+    else:
+        massage = 'I\'ve opened the chest: \n\r Snow:' + str(resp['award']['resource']['money1']) + '\n\r Energy: ' + str(resp['award']['resource']['energy']) + '\n\r Diamonds: ' + str(resp['award']['resource']['cash']) + '\n\r Keys: ' + str(resp['award']['resource']['keys'])
+    el.notifyMe(message)
+
+    resp = el.factoryExchange()
+    message = 'I collected ' + str(resp['collected']) + ' energy.\n\r Now there is ' + str(resp['currentEnergy']) + ' energy'
+    print(message)
+    el.notifyMe(message)
+
+    time.sleep(60*60*4)
+
+
+# responce.json()
+print(responce)
+
 # el.getSessionKey()
 #print(el.sessionKey)
 #resp = el.factoryExchange()
 #print('Got energy: ' + str(resp['collected']) + '\nCurrent energy: ' + str(resp['currentEnergy']))
-resp = el.useEnergy(20)
-print(resp['used'])
+# resp = el.useEnergy(0)
+# print(resp['used'])
+# resp = el.openChest();
+# print(resp['award'])
 # resp = el.totalExchange('energy')
 # print(resp['get'])
